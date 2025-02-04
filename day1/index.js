f5433849d0769f44e4c92665f5f4f60e3f998b47
Amaan-Zaidi4 <amaanzaidi443@gmail.com>
2025-02-04 22:24:26 +0530

    Add files via upload

---
+++ day1/index.js
@@ -0,0 +1,46 @@
+console.log("java script day 1");
+
+var num;//varibale declare.//
+console.log(num); // we got output undefined because in js  code is executing line by line.//
+num=20;// variable assign.//
+
+num=20;//variable assign//
+console.log(num);//we got output 20 because var is working in global scope or also ignore hosting. //
+var num=20;//varibale declare .//
+
+//num1=20;//var assign.//
+//console.log(num1); we got output undefined because let is  working in block scope or also support hosting.//
+//let num1=20;//var declare.//
+
+let num2;//var declare.//
+num2=20;//var assign.//
+console.log(num2);//we got output 20 because let support hosting in hosting first ww have to declare then assign then log.//
+
+let a=10;
+let b=15;
+console.log(a+b);// arithmetic operator addition //
+
+let c=50;
+let d=30;
+console.log(c-d);// arithmetic operator subtraction //
+
+let e=50;
+let f=10;
+console.log(e*f);// arithmetic operator multiplication //
+
+let g=20;
+let h=10;
+console.log(g/h);// arithmetic operator division //
+
+let i=20;
+let j=16;
+console.log(i%j);// arithmetic operator modulus //
+
+let a1=20;
+let b1=30;
+let res= b1==a1;
+console.log(res);
+
+
+
+
