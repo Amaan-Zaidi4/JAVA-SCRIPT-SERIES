f48b09c4954e46af610ed83fa27bcf053841af3c
Amaan-Zaidi4 <amaanzaidi443@gmail.com>
2025-02-15 11:29:08 +0530

    Add files via upload

---
+++ day4/index.js
@@ -0,0 +1,50 @@
+// for loop //
+
+const n=5;
+for( let i=1; i<=n; i++){
+    console.log("i am a programmer");
+    
+}
+
+// for in loop //
+
+const student={
+    name:"monica",
+    class:7,
+    age:12
+}
+for(let key in student){
+    console.log(`${key} =  ${student[key]}`);
+    
+}
+
+// querry 1 //
+ 
+const num=3;
+for (let i=1; i<=10; i++){
+    console.log(`${num} * ${i} = ${num*i} `);
+}
+
+
+
+    // while loop //
+
+    let i=0;
+    while(i<=10){
+        console.log(`value of i is `); 
+        i++;
+    }
+       
+    // do while //
+    
+
+    let a=1
+    do{
+        console.log("value is",a);
+        a++;
+        
+    }
+    while(a<=10);
+        
+
+    
