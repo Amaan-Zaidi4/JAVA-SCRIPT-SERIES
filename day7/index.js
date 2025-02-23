c5407d4ab47e7832257092640a631d60bb2b2ca0
Amaan-Zaidi4 <amaanzaidi443@gmail.com>
2025-02-23 21:56:27 +0530

    Add files via upload

---
+++ day7/index.js
@@ -0,0 +1,37 @@
+// regular function //
+//function a(){
+    //console.log("a");
+
+// arrow function //
+//const a=(b)=>{
+    //console.log();
+    
+//function expression //
+//const  a=function(){
+   // console.log("a");
+    
+//}
+
+function amaan(){ // regular function //
+    console.log("hey...we are developer");
+    
+}
+amaan();
+amaan();
+
+const fun =() =>{ // arrow function //
+    console.log("This is a arrow function");
+    
+}
+fun();
+
+function amaan1(a,b){// parameters //
+    console.log(a,b);
+    
+}
+amaan1(50,60);// arguments //
+
+function amaan2(){
+    alert("hello world");
+}
+amaan2();
